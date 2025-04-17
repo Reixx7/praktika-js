a78734e3a79b25437dd1897025a07763cd53db8a
Reixx7 <reixx23and7@gmail.com>
2025-04-17 10:54:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let box = document.querySelector(".box")
+let bgcolor = prompt("enter body background color");
+let color = prompt("enter text color");
+
+document.body.style.backgroundColor = bgcolor ;
+document.body.style.color = color ;
